679ba1f47870b9cbc458eb404a68c8dc07444f4b
Udeshika Dissanayake <udeshi.ravihari@gmail.com>
2023-01-13 18:19:36 +0000

    change how user variables are stored

--- ProgrammingLanguage/ProgramWindowHandler.cs
+++ ProgrammingLanguage/ProgramWindowHandler.cs
@@ -23,39 +23,72 @@ namespace ProgrammingLanguage
         {
             //string[] text = richText.Lines;
             String[] splitText = new String[maxLimit];
-            
 
-            for(int i = 0; i < text.Length; i++)
+            var variables = defineVariables(text);
+            String[] newData = generatingCommands(text, variables);
+
+            for (int i = 0; i < text.Length; i++)
             {
                 splitText[i] = text[i].ToString().Trim().ToLower();
                 //parser.ParseCommand(splitText[i], true);
             }
-            defineVariables(splitText);
+            
         }
 
-        public void defineVariables(String[] text)
+        public Dictionary<string, string> defineVariables(String[] text)
         {
             String[] store = new String[maxLimit];
             String[] variables = new string[maxLimit];
-            
+            var map = new Dictionary<string, string>();
+
+
 
             foreach (String line in text)
             {
-                //splitText[i] = text[i].ToString().Trim().ToLower();
-                //store variables
                 if (line.Contains("="))
                 {
                     variables = line.Split('=');
-                    store[count] = variables[0];
-                    store[count + 1] = variables[1];
-                    count++;
-                }
+                    variables[0] = variables[0].Trim();
+                    variables[1] = variables[1].Trim();
 
-                if (line.Contains("if") || line.Contains("while"))
-                {
-                    break;
+                    if (map.ContainsKey(variables[0]))
+                    {
+                        map.Add(variables[1], variables[1]);
+                    }
+                    else
+                    {
+                        map.Add(variables[0], variables[1]);
+                    }
                 }
             }
+            return map;
+
         }
+
+        public String[] generatingCommands(String[] text, Dictionary<string, string> data)
+        {
+            
+            for(int i=0; i < text.Length; i++)
+            {
+                String[] commandAndValue = text[i].ToLower().Split(' ');
+
+                if(commandAndValue.Length > 0 && commandAndValue[0].Equals("circle"))
+                {
+                    if (data.ContainsKey(commandAndValue[1]))
+                    {
+                        String s = data[commandAndValue[1]];
+                        String command = "circle " + s;
+                        text[i] = command;
+                    }
+                }
+            }
+
+            return text;
+        }
+        
+        
+        
+
+        
     }
 }
